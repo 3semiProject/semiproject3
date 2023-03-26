a25b51292a1730762df82c3bb434a414bc1fcef1
z469 <64267779+z469@users.noreply.github.com>
2023-03-26 23:43:34 +0900

    sql 작성완료

---
+++ dbscript/deleteSixpackTable.sql
@@ -0,0 +1,111 @@
+drop sequence SEQ_BANNER
+/
+
+drop sequence SEQ_QNA
+/
+
+drop sequence SEQ_QNA_ANSWER
+/
+
+drop sequence SEQ_FAQ
+/
+
+drop sequence SEQ_NOTICE
+/
+
+drop sequence SEQ_EVENT
+/
+
+drop sequence SEQ_FREE
+/
+
+drop sequence SEQ_TIP
+/
+
+drop sequence SEQ_EYEBODY
+/
+
+drop sequence SEQ_BFAF
+/
+
+drop table LOG cascade constraints purge
+/
+
+drop table BANNER cascade constraints purge
+/
+
+drop table QNA_ANSWER cascade constraints purge
+/
+
+drop table QNA cascade constraints purge
+/
+
+drop table FAQ cascade constraints purge
+/
+
+drop table NOTICE cascade constraints purge
+/
+
+drop table EVENT cascade constraints purge
+/
+
+drop table KAKAO cascade constraints purge
+/
+
+drop table NAVER cascade constraints purge
+/
+
+drop table GOOGLE cascade constraints purge
+/
+
+drop table FREE cascade constraints purge
+/
+
+drop table LIKE_FREE cascade constraints purge
+/
+
+drop table TIP cascade constraints purge
+/
+
+drop table LIKE_TIP cascade constraints purge
+/
+
+drop table EYEBODY cascade constraints purge
+/
+
+drop table LIKE_EYEBODY cascade constraints purge
+/
+
+drop table BFAF cascade constraints purge
+/
+
+drop table LIKE_BFAF cascade constraints purge
+/
+
+drop table EAT cascade constraints purge
+/
+
+drop table FOOD cascade constraints purge
+/
+
+drop table ACT cascade constraints purge
+/
+
+drop table MOVE cascade constraints purge
+/
+
+drop table BODY cascade constraints purge
+/
+
+drop table DIARY cascade constraints purge
+/
+
+drop table GOAL cascade constraints purge
+/
+
+drop table MEMBER cascade constraints purge
+/
+
+drop table REC_ACT cascade constraints purge
+/
+
