a62fd19074fee83a974801adabda646183aae15f
chou <asha.jisu@gmail.com>
2023-03-28 15:49:01 +0900

    diary_no int로 수정

--- src/main/java/org/sixpack/semi/act/model/vo/Act.java
+++ src/main/java/org/sixpack/semi/act/model/vo/Act.java
@@ -3,7 +3,7 @@ package org.sixpack.semi.act.model.vo;
 public class Act implements java.io.Serializable{	
 	private static final long serialVersionUID = -1122694301283848688L;
 	//주석만 수정
-	private String diary_no;	//다이어리번호
+	private int diary_no;	//다이어리번호
 	private String act_seq;		//운동 순번
 	private String move_code; 	//운동 일련번호
 	private int act_mm; 		//운동한 시간(m)
@@ -14,7 +14,7 @@ public class Act implements java.io.Serializable{
 		super();	
 	}
 
-	protected Act(String diary_no, String act_seq, String move_code, int act_mm, int act_kcal, String move_name) {
+	protected Act(int diary_no, String act_seq, String move_code, int act_mm, int act_kcal, String move_name) {
 		super();
 		this.diary_no = diary_no;
 		this.act_seq = act_seq;
@@ -30,11 +30,11 @@ public class Act implements java.io.Serializable{
 				+ ", act_kcal=" + act_kcal + ", move_name=" + move_name + "]";
 	}
 
-	public String getDiary_no() {
+	public int getDiary_no() {
 		return diary_no;
 	}
 
-	public void setDiary_no(String diary_no) {
+	public void setDiary_no(int diary_no) {
 		this.diary_no = diary_no;
 	}
 
